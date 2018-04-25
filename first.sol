5b52d421ccadbd63e1bcbc5c14ab1b2d5457e20c
Naresh <nareshtej007@gmail.com>
2018-04-25 16:10:04 +0530

    pushin after a pull

---
+++ first.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.18;
+
+contract First{
+
+
+ string fName;
+ uint age;
+
+   
+   function setInstructor(string _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+   }
+   
+   function getInstructor() public constant returns (string, uint) {
+       return (fName, age);
+   }
+    
+}
